b8487de720092d682a74a78d253a7d9426c7630a
Ankitakusshwaha <ankitakus64@gmail.com>
2025-07-13 20:41:42 +0530

    feature 103 rg (#3)
    Co-authored-by: ankita kushwaha <ankitakus64@gamil.com>

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ resource "azurerm_resource_group" "hello2" {
   
 }
 
+resource "azurerm_resource_group" "hello2" {
+    name = "ramesh"
+    location = "west us"
+  
+}
 resource "azurerm_resource_group" "hello3" {
     name = "ramesh3"
     location = "west us"
